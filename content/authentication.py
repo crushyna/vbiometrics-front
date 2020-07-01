6d02ef7cfa5d3fdeec09ee4f081e8cc19d265e48
Radek M <46869083+crushyna@users.noreply.github.com>
2020-07-02 00:53:44 +0200

    - ['authorized'] disabled in authentication process (2) - added missing Contact Us and About

--- content/authentication.py
+++ content/authentication.py
@@ -10,43 +10,34 @@ class Authentication:
 
     @staticmethod
     def check_email():
-        if 'authorized' in session:
-            return redirect(url_for("record_voice"))
-        else:
-            error = ''
-            try:
-                if request.method == 'POST':
-                    form_merchant_id = request.form['merchant_id']
-                    form_email = request.form['email']
-                    url = f"{ConnectionData.backend_server_address}/check_if_user_exists/{form_merchant_id}/{form_email}"
-                    response = requests.get(url)
-
-                    if response.status_code not in (200, 201):
-                        error = "Email not found! Try again!"
+        error = ''
+        try:
+            if request.method == 'POST':
+                form_merchant_id = request.form['merchant_id']
+                form_email = request.form['email']
+                url = f"{ConnectionData.backend_server_address}/check_if_user_exists/{form_merchant_id}/{form_email}"
+                response = requests.get(url)
+                if response.status_code not in (200, 201):
+                    error = "Email not found! Try again!"
+                    return render_template("authenticate.html", error=error)
+                else:
+                    session['email'] = form_email
+                    session['merchant_id'] = form_merchant_id
+                    randomText = AuthenticatingUser.get_random_text(session['merchant_id'], session['email'])
+                    if randomText['status'] == 'error':
+                        error = str(randomText)
                         return render_template("authenticate.html", error=error)
-
                     else:
-                        session['email'] = form_email
-                        session['merchant_id'] = form_merchant_id
-
-                        randomText = AuthenticatingUser.get_random_text(session['merchant_id'], session['email'])
-                        if randomText['status'] == 'error':
-                            error = str(randomText)
-                            return render_template("authenticate.html", error=error)
-
-                        else:
-                            session['textphrase'] = randomText['message']['data']['textphrase']
-                            session['text_id'] = randomText['message']['data']['text_id']
-                            session['user_id'] = randomText['message']['data']['user_id']
-                            return redirect(url_for("record_voice"))
-
-                gc.collect()
-                return render_template("authenticate.html", error=error)
-
-            except Exception as e:
-                flash(e)
-                error = "Error!"
-                return render_template("authenticate.html", error=error)
+                        session['textphrase'] = randomText['message']['data']['textphrase']
+                        session['text_id'] = randomText['message']['data']['text_id']
+                        session['user_id'] = randomText['message']['data']['user_id']
+                        return redirect(url_for("record_voice"))
+            gc.collect()
+            return render_template("authenticate.html", error=error)
+        except Exception as e:
+            flash(e)
+            error = "Error!"
+            return render_template("authenticate.html", error=error)
 
     @staticmethod
     def record_voice():
